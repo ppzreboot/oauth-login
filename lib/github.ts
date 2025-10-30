a03641796ada2b4eb1a350764c12209c114ec0d2
PPz <ppzreboot@qq.com>
2025-10-30 18:27:07 +0800

    fix: throw error explicitly

--- lib/github.ts
+++ lib/github.ts
@@ -73,21 +73,18 @@ async function get_userinfo_by_token(access_token: string):
         },
     })
     const data = await response.json()
-    switch (data.message) {
-        case 'Bad credentials':
-            return [OAUTH_ERROR__GET_USERINFO.invalid_token, null]
-        case undefined: {
-            const id = data.id + ''
-            const name = data.login
-            const email = data.email
-            if (is_real_str(id) && is_real_str(name) && is_real_str(email))
-                return [0, { id, name, email }]
-        }
-        default:
-            console.error('Unexpected error from github get_userinfo_by_token:')
-            console.error(data)
-            return [OAUTH_ERROR__GET_USERINFO.unknown, null]
+    if (data.message === 'Bad credentials')
+        return [OAUTH_ERROR__GET_USERINFO.invalid_token, null]
+    if (data.message === undefined) {
+        const id = data.id + ''
+        const name = data.login
+        const email = data.email
+        if (is_real_str(id) && is_real_str(name) && is_real_str(email))
+            return [0, { id, name, email }]
     }
+    console.error('Unexpected error from github get_userinfo_by_token:')
+    console.error(data)
+    return [OAUTH_ERROR__GET_USERINFO.unknown, null]
 }
 
 /**
